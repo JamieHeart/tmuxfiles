acbc7954d95bae2816aed91371376aedede35b01
Jamie Hartman <jamie.hartman@act-on.net>
2019-04-15 12:30:39 -0700

    Use vi key-mapping
    This change solves issue #1 by setting the key binding to vi

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-BASHRC_ALIAS='alias tmux="TERM=screen-256color-bce tmux"'
-if [ "0" = "`grep "^${BASHRC_ALIAS}$" ~/.bashrc | wc -l`" ] ; then
-		echo ${BASHRC_ALIAS} >> ${HOME}/.bashrc
-fi
+#BASHRC_ALIAS='alias tmux="TERM=screen-256color-bce tmux"'
+#if [ "0" = "`grep "^${BASHRC_ALIAS}$" ~/.bashrc | wc -l`" ] ; then
+#    echo ${BASHRC_ALIAS} >> ${HOME}/.bashrc
+#fi
 
+if [ -e "${HOME}/.tmux.conf" ] ; then
+    ln -s ${PWD}/tmux.conf ${HOME}/.tmux.conf
+fi
